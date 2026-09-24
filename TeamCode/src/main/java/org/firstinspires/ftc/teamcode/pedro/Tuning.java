20cdede83d8eef7d7f70cdaded6cca5f22751994
gerenaz28 <gerenaz28@student.riverdell.org>
2026-09-24 19:15:19 -0400

    Added PedroPathing Automatic Tuning Sections

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedro/Tuning.java
@@ -1,13 +1,43 @@
 package org.firstinspires.ftc.teamcode.pedro;
 
+import com.pedropathing.revhub.localizers.PinpointLocalizer;
 import com.pedropathing.tuning.autotune.Procedure;
 import com.pedropathing.tuning.autotune.Tuner;
 
+import org.firstinspires.ftc.teamcode.pedro.procedures.ForesightTuner;
 import org.firstinspires.ftc.teamcode.pedro.procedures.MecanumTuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.PinpointTuner;
+import org.firstinspires.ftc.teamcode.pedro.procedures.TwoWheelTuner;
 
 public class Tuning {
+    //Automatic Mecanum Drive Identification (FIRST)
+    //https://pedropathing.com/docs/pathing/tuning/drivetrain/mecanum
+
     @Tuner
     public static Procedure mecanumTuner() {
         return new MecanumTuner();
     }
+
+    //Automatic pinpoint tuning
+    //https://pedropathing.com/docs/pathing/tuning/localization/pinpoint
+
+    /*@Tuner
+    public static Procedure pinpointTuner() {
+        return new PinpointTuner();
+    }*/
+
+    //for two wheel
+    //https://pedropathing.com/docs/pathing/tuning/localization/two-wheel
+
+    /*@Tuner
+    public static Procedure twoWheelTuner() {
+        return new TwoWheelTuner();
+    }*/
+
+    //Automatic Foresight Tuning (LAST)
+    //https://pedropathing.com/docs/pathing/tuning/foresight
+    /*@Tuner
+    public static Procedure foresightTuner() {
+        return new ForesightTuner((hardwareMap) -> new PinpointLocalizer(hardwareMap, Constants.localizerConfig), (hardwareMap) -> new Mecanum(hardwareMap, Constants.drivetrainConfig));
+    }*/
 }
